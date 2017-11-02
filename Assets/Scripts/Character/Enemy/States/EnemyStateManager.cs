8ce850f592220a365512e3eb7635b70ae39c0483
Julio Cesar Soares Falcone Cunha <jcfalcone@MacBook-Pro-de-Julio.local>
2017-11-02 18:21:47 -0400

    Changed enemy folder

---
+++ Assets/Scripts/Character/Enemy/States/EnemyStateManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class EnemyStateManager
+{
+    [SerializeField]
+    List<TemplateState> states = new List<TemplateState>();
+
+    Enemy parent;
+
+	// Use this for initialization
+    void Init (Enemy _parent) 
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
