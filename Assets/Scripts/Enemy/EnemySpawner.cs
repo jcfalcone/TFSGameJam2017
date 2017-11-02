7e1e8c45c1609e3238f7d477d4f88f7d447b514e
talyh <taly.hotimsky@gmail.com>
2017-11-02 12:30:11 -0400

    EnemySpawner generating initial enemies

---
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject _enemyPrefab;
+
+    [SerializeField]
+    private Transform[] _initialSpawnSpots;
+
+    [SerializeField]
+    private Transform[] _relocationSpots;
+
+    void Start()
+    {
+        SpawnLevelEnemies();
+    }
+
+    void SpawnLevelEnemies()
+    {
+        foreach (Transform spot in _initialSpawnSpots)
+        {
+            GameObject.Instantiate(_enemyPrefab, spot.position, spot.rotation);
+        }
+    }
+}
