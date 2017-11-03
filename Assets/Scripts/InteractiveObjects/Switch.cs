c9b7a0630db53123c97178c15bc015cac79ffb49
talyh <taly.hotimsky@gmail.com>
2017-11-03 12:42:03 -0400

    Initial Switch created

--- Assets/Scripts/InteractiveObjects/Switch.cs
+++ Assets/Scripts/InteractiveObjects/Switch.cs
@@ -2,15 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Switch : MonoBehaviour {
+public class Switch : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _affectedObject;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    private float _speed;
+
+    [SerializeField]
+    private Transform _destination;
+
+    void Activate()
+    {
+        InitializeSwitch();
+        ExecuteSwitch();
+        FinalizeSwitch();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            Activate();
+        }
+    }
+
+    void InitializeSwitch()
+    {
+        // animations, sounds, etc
+    }
+
+    void ExecuteSwitch()
+    {
+        StartCoroutine(MoveAffectedObject());
+    }
+
+    void FinalizeSwitch()
+    {
+        // animation, sounds, etc
+    }
+
+    IEnumerator MoveAffectedObject()
+    {
+        Rigidbody affectedObjectedRB = _affectedObject.GetComponent<Rigidbody>();
+        if (affectedObjectedRB)
+        {
+            Vector3 direction = (_destination.position - _affectedObject.position).normalized;
+            while (Vector3.Distance(_affectedObject.position, _destination.position) > 1)
+            {
+                affectedObjectedRB.MovePosition(_affectedObject.position + direction * _speed * Time.deltaTime);
+                yield return new WaitForEndOfFrame();
+            }
+        }
+
+        yield return null;
+    }
 }
